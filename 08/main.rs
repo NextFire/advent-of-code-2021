89942c115a6615656f16d48c8795289db73d7c9f
NextFire <git@yuru.moe>
2021-12-10 11:59:31 +0100

    day 08 part 2

--- 08/main.rs
+++ 08/main.rs
@@ -37,65 +37,131 @@ fn main() {
         // 6 segs: 0, 6, 9
         // 7 segs: 8
 
-        //  1111
-        // 2    3
-        // 2    3
-        //  4444
-        // 5    6
-        // 5    6
-        //  7777
+        //  0000
+        // 1    2
+        // 1    2
+        //  3333
+        // 4    5
+        // 4    5
+        //  6666
+
+        // 1 ->   2  5
+        // 7 -> 0 2  5
+        // 4 ->  123 5
+        // sum  0123 5
+
+        // 2 -> 0 234 6
+        // 3 -> 0 23 56
+        // 5 -> 01 3 56
+
+        // 0 -> 012 456
+        // 6 -> 01 3456
+        // 9 -> 0123 56
 
         const VAL: Vec<&String> = Vec::new();
 
-        for l in &input {
+        let mut result = 0;
+        for line in &input {
             let mut len_tab = [VAL; 8];
 
-            for p in &l[0] {
+            for p in &line[0] {
                 len_tab[p.len()].push(p);
             }
 
             let mut segments = ['_'; 7];
             let mut numbers = ["_"; 10];
 
-            // 2 segments
             numbers[1] = len_tab[2][0];
-            segments[3] = numbers[1].chars().nth(0).unwrap();
-            segments[6] = numbers[1].chars().nth(1).unwrap();
-
-            // 3 segments
             numbers[7] = len_tab[3][0];
+            numbers[4] = len_tab[4][0];
+            numbers[8] = len_tab[7][0];
+
             for c in numbers[7].chars() {
-                if !segments.contains(&c) {
-                    segments[1] = c;
+                if !numbers[1].contains(c) {
+                    segments[0] = c;
                     break;
                 }
             }
 
-            // 4 segments
-            numbers[4] = len_tab[4][0];
-            for c in numbers[4].chars() {
-                if !segments.contains(&c) {
-                    segments[2] = c;
-                    break;
+            for l in &len_tab[5] {
+                let mut count_diff = 0;
+                let mut last_diff = '_';
+                for c in l.chars() {
+                    if !numbers[4].contains(c) && c != segments[0] {
+                        count_diff += 1;
+                        last_diff = c;
+                    }
+                    if count_diff == 1 {
+                        segments[6] = last_diff;
+                    } else if count_diff == 2 {
+                        numbers[2] = l;
+                    }
                 }
             }
-            for c in numbers[4].chars() {
-                if !segments.contains(&c) {
+
+            for c in numbers[2].chars() {
+                if !numbers[4].contains(c) && !segments.contains(&c) {
                     segments[4] = c;
                     break;
                 }
             }
+            for l in &len_tab[5] {
+                for c in l.chars() {
+                    if !numbers[2].contains(c) && !numbers[7].contains(c) {
+                        segments[1] = c;
+                        numbers[5] = l;
+                        break;
+                    }
+                }
+            }
 
-            // 5 segments
+            for l in &len_tab[5] {
+                if !numbers.contains(&l.as_str()) {
+                    numbers[3] = l;
+                    break;
+                }
+            }
 
-            // 6 segments
+            for c in numbers[1].chars() {
+                if numbers[5].contains(c) {
+                    segments[5] = c;
+                } else {
+                    segments[2] = c;
+                }
+            }
 
-            // 7 segments
-            numbers[8] = len_tab[7][0];
+            for c in numbers[8].chars() {
+                if !segments.contains(&c) {
+                    segments[3] = c;
+                    break;
+                }
+            }
 
             println!("{:?}", segments);
+            println!("{:?}", len_tab[6]);
+            for l in &len_tab[6] {
+                if !l.contains(segments[3]) {
+                    numbers[0] = l;
+                } else if !l.contains(segments[2]) {
+                    numbers[6] = l;
+                // else if !l.contains(segments[4]) {   -- lol, it failed somewhere before
+                } else if numbers[9] == "_" {
+                    numbers[9] = l;
+                }
+            }
+            println!("{:?}", numbers);
 
-            break;
+            let mut subresult = 0;
+            for (i, digit) in line[1].iter().enumerate() {
+                for (j, segs) in numbers.iter().enumerate() {
+                    if digit.len() == segs.len() && digit.chars().all(|d| segs.contains(d)) {
+                        subresult += (j as u32) * 10_u32.pow(3 - i as u32);
+                    }
+                }
+            }
+            println!("{}", subresult);
+            result += subresult;
         }
+        println!("{}", result);
     }
 }
