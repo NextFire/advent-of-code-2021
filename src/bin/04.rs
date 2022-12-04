58e5d672cbf416f20ddde026544bd063d23b5949
NextFire <git@yuru.moe>
2022-12-04 00:31:34 -0500

    day 4

---
+++ src/bin/04.rs
@@ -0,0 +1,51 @@
+use std::{fs, ops::RangeInclusive};
+
+#[derive(Debug)]
+struct Pair {
+    first: RangeInclusive<u32>,
+    second: RangeInclusive<u32>,
+}
+
+impl Pair {
+    fn from_line(line: &str) -> Pair {
+        let parts: Vec<_> = line.split(',').collect();
+        let first = range_str_to_range(parts[0]);
+        let second = range_str_to_range(parts[1]);
+        Pair { first, second }
+    }
+}
+
+fn range_str_to_range(range_str: &str) -> RangeInclusive<u32> {
+    let bounds: Vec<_> = range_str.split('-').collect();
+    bounds[0].parse::<u32>().unwrap()..=bounds[1].parse::<u32>().unwrap()
+}
+
+fn main() {
+    let input = fs::read_to_string("inputs/04.txt").unwrap();
+
+    let pairs: Vec<_> = input.trim().lines().map(Pair::from_line).collect();
+
+    let resp1 = pairs
+        .iter()
+        .filter(|pair| {
+            (pair.first.start() >= pair.second.start() && pair.first.end() <= pair.second.end())
+                || (pair.second.start() >= pair.first.start()
+                    && pair.second.end() <= pair.first.end())
+        })
+        .count();
+    println!("{:?}", resp1);
+
+    let resp2 = pairs
+        .iter()
+        .filter(|pair| {
+            (pair.first.start() >= pair.second.start() && pair.first.end() <= pair.second.end())
+                || (pair.second.start() >= pair.first.start()
+                    && pair.second.end() <= pair.first.end())
+                || (pair.first.start() >= pair.second.start()
+                    && pair.first.start() <= pair.second.end())
+                || (pair.second.start() >= pair.first.start()
+                    && pair.second.start() <= pair.first.end())
+        })
+        .count();
+    println!("{:?}", resp2);
+}
